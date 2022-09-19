27ed5be5d7d7abaad49ab19afdcab73a15515a17
claytonjian <claytonjian@gmail.com>
2022-09-19 14:38:45 -0700

    Solved problem 15

---
+++ src/projecteuler/Problem15.java
@@ -0,0 +1,19 @@
+package projecteuler;
+
+import java.math.BigInteger;
+
+public class Problem15 {
+
+	public static void main(String[] args) {
+		// This is a permutation with repetitions question, simply do (n!)/((n - r)!(r!))
+		BigInteger paths = BigInteger.ONE;
+		for(int i = 21; i <= 40; i++) {
+			paths = paths.multiply(BigInteger.valueOf(i));
+		}
+		for(int i = 2; i <= 20; i++) {
+			paths = paths.divide(BigInteger.valueOf(i));
+		}
+		System.out.println(paths);
+	}
+
+}
