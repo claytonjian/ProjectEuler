cd01913a2ec96ed65f74ca8d1615cb24ec72b24e
claytonjian <claytonjian@gmail.com>
2022-09-21 15:04:22 -0700

    Improved name of method

--- src/projecteuler/Problem21.java
+++ src/projecteuler/Problem21.java
@@ -6,16 +6,16 @@ public class Problem21 {
 		// TODO Auto-generated method stub
 		int sum = 0;
 		for(int a = 1; a < 10000; a++) {
-			int b = sumOfFactors(a);
+			int b = sumOfProperDivisors(a);
 			if(b > a) {
-				if(sumOfFactors(b) == a) {
+				if(sumOfProperDivisors(b) == a) {
 					sum += a + b;
 				}
 			}
 		}
 		System.out.println(sum);
 	}
-	public static int sumOfFactors(int num) {
+	public static int sumOfProperDivisors(int num) {
 		int sum = 0;
 		for(int i = 1; i <= (int)Math.sqrt(num); i++) {
 			if(num % i == 0) {
