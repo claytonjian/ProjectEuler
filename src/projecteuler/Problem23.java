df627b84b401b0387e520e18a0030a8a524839e1
claytonjian <claytonjian@gmail.com>
2022-09-21 15:41:16 -0700

    Fixed issue with sumOfProperDivisors method

--- src/projecteuler/Problem23.java
+++ src/projecteuler/Problem23.java
@@ -1,19 +1,23 @@
 package projecteuler;
 
-public class Problem21 {
+import java.util.ArrayList;
+
+public class Problem23 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int sum = 0;
-		for(int a = 1; a < 10000; a++) {
-			int b = sumOfProperDivisors(a);
-			if(b > a) {
-				if(sumOfProperDivisors(b) == a) {
-					sum += a + b;
-				}
+		ArrayList<Integer> abundantNums = new ArrayList<Integer>();
+		for(int i = 1; i <= 28123; i++) {
+			if(isAbundant(i)) {
+				abundantNums.add(i);
 			}
 		}
-		System.out.println(sum);
+	}
+	public static boolean isAbundant(int num) {
+		if(sumOfProperDivisors(num) > num) {
+			return true;
+		}
+		return false;
 	}
 	public static int sumOfProperDivisors(int num) {
 		int sum = 0;
